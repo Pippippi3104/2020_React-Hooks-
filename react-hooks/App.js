5f017db96dfdc14734e0a501078b1976578f16e5
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2020-12-31 00:43:12 +0900

    # finish editing lesson08

--- react-hooks/App.js
+++ react-hooks/App.js
@@ -2,16 +2,18 @@ import React from "react";
 import "./App.css";
 import Counter from "./component/Counter";
 import CounterHook from "./component/CounterHook";
+import EffectHook from "./component/EffectHook";
 import FormHook from "./component/FormHook";
 import ItemHook from "./component/ItemHook";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      {/* <Counter />
       <CounterHook />
       <FormHook />
-      <ItemHook />
+      <ItemHook /> */}
+      <EffectHook />
     </div>
   );
 };
--- react-hooks/component/EffectHook.js
+++ react-hooks/component/EffectHook.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function EffectHook() {
+    const [count, setCount] = useState(0)
+    const [name, setName] = useState("")
+
+    useEffect(() => {
+        document.title = `クリック数: ${count} 回`
+        console.log("render")
+    }, [count])
+
     return (
         <div>
-            
+            <p>クリック数: {count} 回</p>
+            <p>名前: {name}</p>
+            <input
+                type="text"
+                value={name}
+                onChange={(e => setName(e.target.value))}
+            />
+            <button onClick={() => setCount(count + 1)}>カウント +</button>        
         </div>
     )
 }
